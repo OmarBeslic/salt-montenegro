4c1afe39bfb07269cbf9a3f642663e228b2bd09c
OmarBeslic <beslic.omar33@gmail.com>
2023-05-09 16:56:50 +0200

    Initial setup

---
+++ src/Theme/breakpoints.js
@@ -0,0 +1,14 @@
+const breakpoints = {
+    values: {
+      phone: 576,
+      tabletSmall: 720,
+      tablet: 1024,
+      laptop: 1600,
+      laptopSmall: 1366,
+      desktop: 2000, // -- by default
+      desktopMedium: 4096, // -- 4k
+      desktopLarge: 7690, // -- 8k
+    },
+  };
+  export default breakpoints;
+  
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { lazy, Suspense } from "react";
+import { useRoutes } from "react-router-dom";
+
+export default function Router() {
+  return useRoutes([
+    // { path: "/", element: <LoginPage /> },
+    // { path: "/administration", element:  },
+  ]);
+}
